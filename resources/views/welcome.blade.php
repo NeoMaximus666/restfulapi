b509b39f89a33616660d317f76bb3b3cb0bde2d9
NeoMaximus666 <maximgreenyuk@gmail.com>
2019-04-12 01:07:23 +0300

    setting logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,8 @@
                     Laravel
                 </div>
 
+                <img src="{{ Voyager::image(setting('site.logo')) }}" alt="">
+
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
